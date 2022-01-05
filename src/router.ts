f22e38eda55232805d041cd32361da167497ef7d
Megatank58 <tanumegatank@gmail.com>
2022-01-05 03:29:44 +0000

    feat: change title per page

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,15 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import routes from 'virtual:generated-pages';
 
-export default createRouter({
+const router = createRouter({
 	history: createWebHashHistory(),
 	routes,
 });
+
+router.beforeEach((to, from, next) => {
+	const title = to.path.split('/')[to.path.split('/').length - 1];
+	document.title = title && title.length !== 0 ? `${title} - discord.js` : 'discord.js';
+	next();
+});
+
+export default router;
